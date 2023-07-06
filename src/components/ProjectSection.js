a48efc35e0f6cbf31758b6a778c602f1769a07ab
Will Dinauer <77174175+WillDinauer@users.noreply.github.com>
2023-07-06 11:43:09 -0400

    added more project info and images

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -12,12 +12,29 @@ function ProjectSection() {
             <Project 
               src={require('../images/Cedar.png')}
               text="Cedar Mobile App"
-              label='React-Native'
+              label='React Native, Express.js. MongoDB'
             />
             <Project 
-              src={require('../images/Cedar.png')}
-              text="Cedar Mobile App"
-              label='React-Native'
+              src={require('../images/TSE.png')}
+              text="Tiny Search Engine"
+              label='C, Makefiles, Unix'
+            />
+          </ul>
+          <ul className="projects__items">
+            <Project 
+              src={require('../images/x86_code_generator.PNG')}
+              text="X86 Code Generator"
+              label='C++'
+            />
+            <Project 
+              src={require('../images/HMM.PNG')}
+              text="Part of Speech Recognizer"
+              label='Java'
+            />
+            <Project 
+              src={require('../images/SpaceWizardPhoto.JPG')}
+              text="Space Wizards VR"
+              label='Unity, C#'
             />
           </ul>
         </div>
